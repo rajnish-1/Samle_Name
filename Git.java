a1b5054bd71387f958aa3a88ca7a9aaad3f5edf1
rajnish-1 <rk4275465@gmail.com>
2022-12-09 01:14:55 -0800

    Changes made in Git.java updated m1() method

--- Git.java
+++ Git.java
@@ -3,5 +3,8 @@ package com.company;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Welcome to github session");
+        public void m1(){
+            System.out.println("I have created method name m1.....chnages ha sto made by harsish");
+        }
     }
 }
